0c2c58a630c9ce350e0c5f59ee48294bc0a0e138
Kevin Jakubczak <Kevin.M.Jakubczak@sherwin.com>
2022-03-25 12:02:50 -0400

    Adding antsibull.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,11 @@
 
 time=$(date)
 ansiblepath=$(which ansible)
+antsibullpath=$(which antsibull-docs)
 echo "Hello $1"
 echo "$ansiblepath"
+echo "$antsibullpath"
+
 echo "::set-output name=time::$time"
 
 echo "::set-output name=time::$ansiblepath"
