e82d2e261170f3ba06c29f1fdf1616b6c6aaee84
Kevin Jakubczak <Kevin.M.Jakubczak@sherwin.com>
2022-03-25 12:06:03 -0400

    Adding github_workspace.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ antsibullpath=$(which antsibull-docs)
 echo "Hello $1"
 echo "$ansiblepath"
 echo "$antsibullpath"
-
+echo "$GITHUB_WORKSPACE"
 echo "::set-output name=time::$time"
 
 echo "::set-output name=time::$ansiblepath"
