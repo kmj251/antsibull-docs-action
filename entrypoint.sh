18ee35b49b552a0d699386a4fc3f64ca66092c2d
Kevin Jakubczak <Kevin.M.Jakubczak@sherwin.com>
2022-03-25 15:48:18 -0400

    CHowing github_workspace.

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,7 @@ collection=$(awk '/^name:/ {print $2}' $GITHUB_WORKSPACE/galaxy.yml)
 ansible-galaxy collection install git+${my_github_url}
 #cp -R docsrc/source/* /tmp/docsrc/source/.
 #antsibull-docs collection --use-current --squash-hierarchy --dest-dir ./docsrc/source $namespace.$collection
+chown -R root:root $GITHUB_WORKSPACE
 antsibull-docs collection --use-current --squash-hierarchy --dest-dir ./docsrc/source $namespace.$collection
 #sphinx-build /tmp/docsrc/source ./docs
 sphinx-build ./docsrc/source ./docs
