ddb6b5dd76ffb9e299b05d0c2422b3eb38ab0e6d
Kevin Jakubczak <Kevin.M.Jakubczak@sherwin.com>
2023-05-09 14:44:59 -0400

    Add --no-deps to ansible-galaxy install.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@
 namespace=$(awk '/^namespace/ {print $2}' $GITHUB_WORKSPACE/galaxy.yml)
 collection=$(awk '/^name:/ {print $2}' $GITHUB_WORKSPACE/galaxy.yml)
 ls -ralt $GITHUB_WORKSPACE/.git
-ansible-galaxy collection install git+file://$GITHUB_WORKSPACE/.git
+ansible-galaxy collection install git+file://$GITHUB_WORKSPACE/.git --no-deps
 mkdir -p ./docsrc/source
 chown -R root:root $GITHUB_WORKSPACE
 antsibull-docs collection --use-current --squash-hierarchy --dest-dir ./docsrc/source $namespace.$collection
